1308088f3bf2c19e81e4ad37e4ac0391667c3540
Texas1000 <microondasconqueso69@gmail.com>
2021-10-11 00:27:48 -0400

    Added Conversion to Integer and added a optional default value

--- src/DiscordControlUI/LuaStuff.cs
+++ src/DiscordControlUI/LuaStuff.cs
@@ -23,13 +23,22 @@ namespace ImGui.NET.SampleProgram
 
     public class Convertion
     {
-        public static float Float(string s)
+        public static float Float(string s, float Default = 0)
         {
             if(float.TryParse(s,System.Globalization.NumberStyles.Float,System.Globalization.CultureInfo.InvariantCulture,out var r))
             {
                 return r;
             }
-            return ulong.Parse("0");
+            return Default;
+        }
+
+        public static int Integer(string s, int Default = 0)
+        {
+            if(int.TryParse(s,out var r))
+            {
+                return r;
+            }
+            return Default;
         }
     }
 }
