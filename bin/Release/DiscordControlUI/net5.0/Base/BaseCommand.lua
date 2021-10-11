52fcb600d18628371df474d5e33c611c0b6a0adc
Texas1000 <microondasconqueso69@gmail.com>
2021-10-10 21:34:13 -0400

    added git ignore

---
+++ bin/Release/DiscordControlUI/net5.0/Base/BaseCommand.lua
@@ -0,0 +1,11 @@
+function Execute(context,args)
+
+end
+
+function CommandName()
+	return "COMMAND NAME"
+end
+
+function CommandDescription()
+	return "COMMAND DESCRIPTION"
+end
---
+++ bin/Release/DiscordControlUI/net5.0/Base/BaseProject/events.lua
@@ -0,0 +1,7 @@
+function OnMessage(client,messageEventArgs)
+
+end
+
+function OnReady(client,readyEventArgs)
+
+end
